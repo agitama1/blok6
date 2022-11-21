d2866bad407a3ced0411f5be985d75a8eb70dcc6
agitama1 <93070268+agitama1@users.noreply.github.com>
2022-11-21 14:49:30 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "blok6.py",
+    version = "0.1",
+    py_modules = ['blok6'],
+    url = "https://github.com/agitama1/agitama1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
